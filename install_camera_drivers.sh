d70ec534e6574fe5d40aa2251c505f5b71c5a11d
cxa468 <cxa468@student.bham.ac.uk>
2017-10-17 16:21:27 +0100

    Fixed install directory location and typos.

--- install_camera_drivers.sh
+++ install_camera_drivers.sh
@@ -4,7 +4,11 @@ if [[ $EUID > 0 ]]
   then echo "Please run as root"
   exit
 fi
-echo "Downloading driver zip to current directory."
+
+echo "Switching to ~/Documents to install there."
+cd ~/Documents/
+
+echo "Downloading driver zip."
 wget http://www.orbbec3d.net/Tools_SDK_OpenNI/2-Linux.zip
 
 echo "Unzipping and changing directory."
@@ -16,5 +20,8 @@ cd OpenNI-Linux-x64-2.3
 echo "Changing permissions and executing install script."
 chmod +x install.sh
 ./install.sh
+chmod +x ./Samples/Bin/SimpleRead
+chmod +x ./Samples/Bin/SimpleViewer
 
-echo "You can check whether this has installed correctly by connecting the camera and running SampleView or SampleRead in 2-Linux/OpenNI-Linux-x64-2.3/Samples/Bin/"
+
+echo "You can check whether this has installed correctly by connecting the camera and running SimpleView or SimpleRead in 2-Linux/OpenNI-Linux-x64-2.3/Samples/Bin/"
