45b303f281ac2c498bcd7a0460642df43bb491d6
Sebastian Dica <dicasebastian@gmail.com>
2017-10-18 17:44:20 +0100

    Refactoring

--- src/camera_modules/barcode_node.py
+++ src/camera_modules/barcode_node.py
@@ -6,48 +6,48 @@ import numpy as np
 def identify_barcode(frame):
     return None
 
-"""
-    Code from
-    https://www.pyimagesearch.com/2014/11/24/detecting-barcodes-images-python-opencv/
-"""
-
 
 def detect(image):
-    # convert the image to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    """
+        Barcode detection using gradients code below is from:
+        https://www.pyimagesearch.com/2014/11/24/detecting-barcodes-images-python-opencv/
+    """
 
-    # compute the Scharr gradient magnitude representation of the images
-    # in both the x and y direction
-    gradX = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=1, dy=0, ksize=-1)
-    gradY = cv2.Sobel(gray, ddepth=cv2.cv.CV_32F, dx=0, dy=1, ksize=-1)
+    # Convert the image to grayscale
+    grey = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # subtract the y-gradient from the x-gradient
-    gradient = cv2.subtract(gradX, gradY)
+    # Compute the Scharr gradient magnitude representation of the images
+    # In both the x and y direction
+    grad_x = cv2.Sobel(grey, ddepth=cv2.cv.CV_32F, dx=1, dy=0, ksize=-1)
+    grad_y = cv2.Sobel(grey, ddepth=cv2.cv.CV_32F, dx=0, dy=1, ksize=-1)
+
+    # Subtract the y-gradient from the x-gradient
+    gradient = cv2.subtract(grad_x, grad_y)
     gradient = cv2.convertScaleAbs(gradient)
 
-    # blur and threshold the image
+    # Blur and threshold the image
     blurred = cv2.blur(gradient, (9, 9))
     (_, thresh) = cv2.threshold(blurred, 225, 255, cv2.THRESH_BINARY)
 
-    # construct a closing kernel and apply it to the thresholded image
+    # Construct a closing kernel and apply it to the thresholded image
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (21, 7))
     closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-    # perform a series of erosions and dilations
+    # Perform a series of erosions and dilations
     closed = cv2.erode(closed, None, iterations=4)
     closed = cv2.dilate(closed, None, iterations=4)
 
-    # find the contours in the thresholded image
+    # Find the contours in the thresholded image
     (cnts, _) = cv2.findContours(closed.copy(),
                                  cv2.RETR_EXTERNAL,
                                  cv2.CHAIN_APPROX_SIMPLE)
 
-    # if no contours were found, return None
+    # If no contours were found, return None
     if len(cnts) == 0:
         return None
 
-    # otherwise, sort the contours by area and compute the rotated
-    # bounding box of the largest contour
+    # Otherwise, sort the contours by area and compute the rotated
+    # Bounding box of the largest contour
     c = sorted(cnts, key=cv2.contourArea, reverse=True)[0]
     rect = cv2.minAreaRect(c)
     box = np.int0(cv2.cv.BoxPoints(rect))
@@ -55,5 +55,5 @@ def detect(image):
     # Draw the box on the image
     cv2.drawContours(image, [box], -1, (255, 0, 255), 2)
 
-    # return the bounding box of the barcode
+    # Return the bounding box of the barcode
     return identify_barcode(box)
--- src/camera_modules/camera_node.py
+++ src/camera_modules/camera_node.py
@@ -1,31 +1,32 @@
-import numpy as np
 import cv2
 import barcode_node
 
+# 0 should be your in-build camera
+# 1 should be your external plugged-in camera
+# TODO : Move number to config
+SELECTED_VIDEO_INDEX = 1
 
-def get_data_from_camera(camera_checker, color):
+def get_data_from_camera(camera_checker, colour):
 
-    # 0 should be your in-build camera
-    # 1 should be your external plugged-in camera
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(SELECTED_VIDEO_INDEX)
 
     # Force change the resolution if you want
     # cap.set(3, 1280.)
     # cap.set(4, 720.)
 
     data = None
-    while(data is None):
+    while data is None:
         # Get the current frame
         ret, frame = cap.read()
 
         # Apply the method
         data = camera_checker(frame)
 
-        # Choose whether or not you want color in your image
-        # E.g. barcode scanning is done best on gray scale images
-        if(color is False):
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            cv2.imshow('frame', gray)
+        # Choose whether or not you want colour in your image
+        # E.g. barcode scanning is done best on grey scale images
+        if colour is False:
+            grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('frame', grey)
         else:
             cv2.imshow('frame', frame)
 
@@ -42,6 +43,3 @@ def get_data_from_camera(camera_checker, color):
     # Return whatever data was found through the camera_checker method
     # passed for further planning/decision making
     return data
-
-if __name__ == "__main__":
-    get_data_from_camera(barcode_node.detect, True)
