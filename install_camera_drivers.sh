4521c33a808eba6953d7375d5d5fbf9882a28ce6
cxa468 <cxa468@student.bham.ac.uk>
2017-10-22 15:55:26 +0100

    Fixed file ownership and typos.

--- install_camera_drivers.sh
+++ install_camera_drivers.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-if [[ $EUID > 0 ]]
-  then echo "Please run as root"
+if [[ $EUID > 0 ]] 
+  then
+  echo "Please run as root." >&2
   exit
 fi
 
@@ -10,18 +11,19 @@ cd ~/Documents/
 
 echo "Downloading driver zip."
 wget http://www.orbbec3d.net/Tools_SDK_OpenNI/2-Linux.zip
+chown $SUDO_USER:$SUDO_USER 2-Linux.zip 
 
 echo "Unzipping and changing directory."
 unzip 2-Linux.zip
 cd 2-Linux
 unzip OpenNI-Linux-x64-2.3.zip
+chown -R $SUDO_USER:$SUDO_USER ../2-Linux 
 cd OpenNI-Linux-x64-2.3
 
 echo "Changing permissions and executing install script."
 chmod +x install.sh
 ./install.sh
-chmod +x ./Samples/Bin/SimpleRead
-chmod +x ./Samples/Bin/SimpleViewer
-
+chmod +x Samples/Bin/SimpleRead
+chmod +x Samples/Bin/SimpleViewer
 
 echo "You can check whether this has installed correctly by connecting the camera and running SimpleView or SimpleRead in 2-Linux/OpenNI-Linux-x64-2.3/Samples/Bin/"
