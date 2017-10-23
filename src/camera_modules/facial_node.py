e52bee08dbbf80dedfdc27739fc2ddf48aa9d573
Sebastian Dica <dicasebastian@gmail.com>
2017-10-23 15:50:53 +0100

    Refactoring and LFS

--- src/camera_modules/facial_node.py
+++ src/camera_modules/facial_node.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 import face_recognition  # sudo pip install face_recognition
 import cv2
 import facial_utils
@@ -8,18 +7,23 @@ import facial_utils
 saved_encodings = facial_utils.get_known_faces()
 # TODO Add these in the configuration file
 frame_count = 0
-frame_threshold = 30
+FRAME_THRESHOLD = 30
 
 
 def identify(image):
+    """
+    Identify faces in a given frame by associating them with
+    filenames in the /known_faces/ directory.
+    """
+    global saved_encodings
     face_locations = face_recognition.face_locations(image)
     unknown_encodings = face_recognition.face_encodings(image)
 
     # Draw the recognized locations on the frame
-    for (top, right, bottom, left) in face_locations:
+    for top, right, bottom, left in face_locations:
         cv2.rectangle(image, (left, top), (right, bottom), (0, 0, 255), 2)
 
-    indeces = []
+    indices = []
 
     # For each face encoding
     for unknown_encoding in unknown_encodings:
@@ -28,32 +32,38 @@ def identify(image):
                                                  unknown_encoding)
 
         # Identify the indeces of the matched faces
-        indeces = indeces + [index for index, flag in enumerate(results)
+        indices += [index for index, flag in enumerate(results)
                              if flag]
 
     # Get rid of any duplicates
-    indeces = set(indeces)
+    indices = set(indices)
 
-    # Extract the original filenames using the indeces of the matched faces
-    if len(indeces) > 0:
+    # Extract the original filenames using the indices of the matched faces
+    if len(indices) > 0:
         identified = []
-        for index in indeces:
+        for index in indices:
             identified.append(facial_utils.get_matching_name(index))
-        print "Identified: " + str(identified)
+        print "Identified: ", identified
 
     return None
 
 
 def detect(image):
-    global frame_count
+    """
+    Only attempt to recognize a face in a frame every time
+    the frame_count reaches the FRAME_THRESHOLD. When the threshold
+    is reached, the identification process is carried out on the
+    selected frame. Consequently, the frame_count is reset.
+    """
+    global frame_count, FRAME_THRESHOLD
     data = None
 
     # Only pick up every other frame
-    if (frame_count > frame_threshold):
+    if frame_count > FRAME_THRESHOLD:
         data = identify(image)
         frame_count = 0
 
-    frame_count = frame_count + 1
+    frame_count += 1
     return data
 
 
