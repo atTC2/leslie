9d9e3fa1e621889eb294902d2a2e9e5585331dd2
rrama <ben_durrans@hotmail.co.uk>
2017-11-05 15:43:03 +0000

    Move states and actions out of the state machine
    Refactor the state machine
    Deal with the launch file situation
    Fix broken `state_test.py`
    Add Linguist languages for `launch` and `test` files

--- test/state_test.py
+++ test/state_test.py
@@ -5,7 +5,7 @@ import rostest
 import rospy
 import json
 from std_msgs.msg import String
-from state_machine.state_machine import StateIDs, ActionIDs
+from state_machine import states, actions
 
 # Had to make these global so we could write tests
 callback_called = 0
@@ -22,26 +22,26 @@ class StateMachineTestClass(unittest.TestCase):
         global result_state_id
         callback_called = 0
 
-        def callback(state_msg):
+        def state_callback(state_msg):
             global callback_called
             global result_state_id
             callback_called += 1
             state = json.loads(state_msg.data)
             result_state_id = state['id']
 
-        rospy.Subscriber('/state', String, callback, queue_size=1)
+        rospy.Subscriber('/state', String, state_callback, queue_size=1)
         pub = rospy.Publisher('/action', String, queue_size=1)
         # Ensures the publisher and subscriber have had time to register
         rospy.sleep(1)
         action = {}
-        action['id'] = ActionIDs.CALLED_OVER
+        action['id'] = actions.CALLED_OVER
         action['data'] = {}
 
         pub.publish(json.dumps(action))
         rospy.sleep(1)
         self.assertEqual(callback_called, 1,
                          'Callback not called correct amount of times (' + str(callback_called) + ')')
-        self.assertEqual(result_state_id, StateIDs.MOVE_TO_TABLE, 'State not updated correctly (' + result_state_id + ')')
+        self.assertEqual(result_state_id, states.MOVE_TO_TABLE, 'State not updated correctly (' + result_state_id + ')')
 
 
 if __name__ == '__main__':
