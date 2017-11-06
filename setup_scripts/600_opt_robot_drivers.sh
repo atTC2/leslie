74d3b12cfc553a26daa360172eae54010f014d62
rrama <ben_durrans@hotmail.co.uk>
2017-11-06 19:55:47 +0000

    Add install script to run on the robot
    Split launch scripts into base, stage and robot

---
+++ setup_scripts/600_opt_robot_drivers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/Documents/
+svn checkout https://codex.cs.bham.ac.uk/svn/int-robot/course/socspioneer
+echo '
+# Make SoCS Pioneer visible
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/Documents/socspioneer' >> ~/.bashrc
+
+sudo apt-get install -qq ros-indigo-hokuyo3d ros-indigo-hokuyo-node \
+ros-indigo-p2os-doc ros-indigo-p2os-launch ros-indigo-p2os-teleop \
+ros-indigo-p2os-driver ros-indigo-p2os-msgs \
+ros-indigo-joy ros-indigo-joystick-drivers \
+ros-indigo-joy-listener ros-indigo-joy-teleop \
+ros-indigo-joy-mouse
