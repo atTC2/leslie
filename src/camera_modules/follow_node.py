475f46a150fa4448a3d151d5ea68e99a8771f0df
Hung Hoang <vnh484@student.bham.ac.uk>
2017-11-21 17:06:57 +0000

    Refactored most functions to util except those directly called by node. started documenting

--- src/camera_modules/follow_node.py
+++ src/camera_modules/follow_node.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python
 
 import json
-import math
 import numpy
 from cv_bridge import CvBridge
 from threading import Lock, Thread
 
 import cv2
 import imutils
-import matplotlib.pyplot as plt
 import rospy
 from geometry_msgs.msg import Twist
-from imutils.object_detection import non_max_suppression
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
 
 from state_machine import states, state_util
-from util_modules import utils_detect
+from util_modules.utils_detect import detect_closest_to_thief, init_distro, update_distro
 
-history = []
-history_rgb = []
-max_history = 10
-latest_rekt_global = None
-global_lock = Lock()
-distro_lock = Lock()
-distro = []
-distro_size = 400
+# --- array variables holding data on images current and previous ---
+depth_history = []  # history of depth images
+history_rgb = []  # history of rgb images (same raw image as depth)
+max_history = 10  # max history size
+# -------------------------------------------
 
-colour_diff_threshold = 40
+latest_rect_global = None  # most up to date rect describing thief
+global_lock = Lock()  # Thread lock for image showing
+locked_colour = None  # colour of thief's shirt
 
-locked_colour = None
+# --------------------------------------------------
+# -- we represent the system's `belief` of the location of the thief
+# -- as a probability distribution over the possible angles
+
+distro_lock = Lock()  # Thread lock for updating 'belief' distribution
+distro = []  # distribution representing `belief`
+distro_size = 400  # size of distribution
+# --------------------------------------------------
+
 last_degree = None
 fov = 120.0
 
@@ -39,136 +43,36 @@ odom_pub = rospy.Publisher('cmd_vel', Twist, queue_size=100)
 state_id = state_util.get_start_state()
 
 
-def detect_angle_to_person(image, rectangle):
-    global distro_size, fov
-
-    top_left = rectangle[0][0]
-    bottom_right = rectangle[1][0]
-    if (bottom_right >= distro_size):
-        bottom_right = distro_size - 1
-
-    mid_image = image.shape[1] / 5.0
-
-    centre = ((top_left + bottom_right) / 2)
-
-    fixed_angle = 1
-
-    if centre <= mid_image:
-        return -fixed_angle
-    if centre > mid_image and centre < (mid_image)*4:
-        return 0
-    if centre >= (mid_image * 4):
-        return fixed_angle
-
-
-def detect_people(unmodified_image):
-    global latest_rekt_global, locked_colour
-
-    hog = cv2.HOGDescriptor()
-    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    drawn_on_image = unmodified_image.copy()
-
-    # detect people in the image
-    (rects, weights) = hog.detectMultiScale(unmodified_image, winStride=(6, 6),
-                                            padding=(16, 16), scale=1.05)
-    '''
-    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-        padding=(8, 8), scale=1.05)
-    '''
-    # draw the original bounding boxes
-    # for (x, y, w, h) in rects:
-    #    cv2.rectangle(drawn_on_image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-    # detect_angle_to_person(image, ((x, y), (x+w, y + h)))
-
-    angle = 0
-    lost = True
-    largest_rekt = None
-    # apply non-maxima suppression to the bounding boxes using a
-    # fairly large overlap threshold to try to maintain overlapping
-    # boxes that are still people
-    closest_rekt = None
-    if len(rects) != 0:
-        rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-
-        closest_colour_diff = 99999999  # some max number
-
-        # draw the final bounding boxes
-        for (xA, yA, xB, yB) in pick:
-            # print (xA, yA, xB, yB)
-            new_xA = (xB - xA) / 3 + xA
-            new_yA = (yB - yA) / 4 + yA
-            new_xB = xB - (xB - xA) / 3
-            new_yB = yB - (yB - yA) / 2
-            if new_xB >= 400:
-                new_xB = 399
-            if new_yB >= 300:
-                new_yB = 299
-            mode_colour = utils_detect.make_histogram(unmodified_image, new_xA, new_yA, new_xB, new_yB, False)
-            print 'mode', mode_colour
-            # avg_rect_colour = utils_detect.detect_avg_color2(drawn_on_image, [new_xA, new_yA], [new_xB, new_yB])
-            cv2.rectangle(drawn_on_image, (new_xA, new_yA), (new_xB, new_yB), (0, 255, 255), 2)
-            cv2.rectangle(drawn_on_image, (xA, yA), (xB, yB), (0, 255, 0), 2)
-            colour_diff = utils_detect.euclidian_colour_diff(mode_colour, locked_colour)
-            print 'diff', colour_diff
-            if colour_diff < closest_colour_diff:
-                closest_colour_diff = colour_diff
-                closest_rekt = ((xA, yA), (xB, yB))
-                latest_rekt_global = closest_rekt
-                angle = detect_angle_to_person(unmodified_image, ((xA, yA), (xB, yB)))
-
-        lost = closest_colour_diff > colour_diff_threshold
-
-    return drawn_on_image, angle, lost, closest_rekt
-
-
-def get_distance(((xA, yA), (xB, yB))):
-    global history
-    saved_history = history[:]
-    min_distance = 666666
-    max_distance = 0
-    avg_distance = 0
-    for depth_image in saved_history:
-        avgX = (xB + xA) / 2
-        avgY = (yB + yA) / 7
-        x_diff_offset = (xB - xA) * 5 / 100
-        y_diff_offset = (yB - yA) * 15 / 100
-
-        xB = avgX + x_diff_offset
-        xA = avgX - x_diff_offset
-        yB = 3 * avgY + y_diff_offset
-        yA = 3 * avgY - y_diff_offset
-        depth_image = depth_image[yA:yB, xA:xB]
-
-        values = filter(lambda a: a != 0, depth_image.flatten())
-        if (len(values) > 0):
-            avg_distance = numpy.array(values).mean()
-            min_distance = min(values)
-            max_distance = max(values)
-
-    if avg_distance == 666666:
-        return 0
-    return avg_distance / 1000.0
-
-
 def save_distance(img):
-    global max_history, history, global_lock
+    '''
+    Saves the depth image to history.
+    If the history is full, pop the oldest image off.
 
+    :param img: depth image
+    :type img: sensor_msgs.msg.Image
+    '''
+    global max_history, depth_history, global_lock
+
+    # use a cv bridge to convert depth image into c2 image
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(img, "16UC1")
+    # resize the image width to be 400px or less, don't need full fov
     cv_image = imutils.resize(cv_image, width=min(400, cv_image.shape[1]))
     cv_image = numpy.array(cv_image, dtype=numpy.float32)
-    history.append(cv_image)
-    if (len(history) > max_history):
-        history = history[1:]
+    # add to history
+    depth_history.append(cv_image)
+    # pop oldest if reach max size of history
+    if(len(depth_history) > max_history):
+        depth_history = depth_history[1:]
 
+    # create a colour image out of the depth image to show
     cv_image = cv_image.copy()
     cv_image = cv2.normalize(cv_image, cv_image, 0, 1, cv2.NORM_MINMAX)
     cv_image = cv2.cvtColor(cv_image, cv2.COLOR_GRAY2RGB)
 
-    if (latest_rekt_global is not None):
-        ((xA, yA), (xB, yB)) = latest_rekt_global
+    # draw the bounding box around the thief if available
+    if (latest_rect_global is not None):
+        ((xA, yA), (xB, yB)) = latest_rect_global
         cv2.rectangle(cv_image, (xA, yA), (xB, yB), (0, 0, 255), 2)
 
     # print ('DEPTH:', cv_image.shape)
@@ -178,6 +82,13 @@ def save_distance(img):
 
 
 def rgb_color(img):
+    '''
+    Convert the Depth image into an RGB image.
+    RGB image is used for shirt colour and define angle to person
+
+    :param img: image to convert
+    :type img: nump.array/cv2.image
+    '''
     global history_rgb, max_history, global_lock
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(img, "passthrough")
@@ -188,20 +99,32 @@ def rgb_color(img):
 
 
 def decide_on_thief_status():
+    '''
+    Detects where the thief is from camera input as angle and distance.
+    From the detection information, update the probability distribution
+    that reflects the 'belief' of potential angle and distance
+    the thief is actually away from the robot.
+
+    If the camera no longer detects anyone, ensure that is consistent for `x`
+    frames. Camera no longer detects if person is too close or too far
+    If the last `distance` detected 
+    '''
+    global locked_colour
     counter = 0
     timeout = 0
 
     while state_id == states.ALARM:
-        global history_rgb, max_history, latest_rekt_global, locked_colour
+        global history_rgb, max_history, latest_rect_global, locked_colour
         if (len(history_rgb) >= max_history):
             img = history_rgb[len(history_rgb) - 1]
 
             # Apply the method
-            drawn_on_image, angle, lost, largest_rekt = detect_people(img)
+            drawn_on_image, angle, lost, largest_rekt = detect_closest_to_thief(img, locked_colour)
 
             if largest_rekt is not None:
                 ((xA, yA), (xB, yB)) = largest_rekt
-                update_distro((xB - xA) / 2 + xA, 10)
+                with distro_lock:
+                    update_distro((xB - xA) / 2 + xA, 10)
                 cv2.rectangle(drawn_on_image, (xA, yA), (xB, yB), (0, 0, 255), 2)
             # Make frame
             if lost:
@@ -211,8 +134,10 @@ def decide_on_thief_status():
                 with distro_lock:
                     where_do_i_think = distro.index(max(distro))
 
+                # Following code is for following people which is not completed
                 # angle = detect_angle_to_person(drawn_on_image, ((where_do_i_think - 1, 0), (where_do_i_think + 1, 300)))
                 # waypoint_pub.publish(json.dumps({'id': 'FOLLOW_PERP', 'data': {'angle': angle, 'distance': 0.5}}))
+                
                 cv2.rectangle(drawn_on_image, (where_do_i_think - 1, 0), (where_do_i_think + 1, 300), (255, 0, 0), 2)
             with global_lock:
                 cv2.imshow('actualimage', drawn_on_image)
@@ -227,84 +152,6 @@ def decide_on_thief_status():
             if counter == 200:
                 print 'Counter limit'
                 return
-
-
-def angle_to_prob(angle):
-    global distro_size, distro
-    # print msg.twist.twist.linear.x
-
-    notch = distro_size / fov
-
-    if (angle == 0):
-        return max_in_range(int(distro_size / 2), 20, distro, distro_size)
-    if (angle > 0):
-        return max_in_range(int(distro_size / 2 - angle * notch), 20, distro, distro_size)
-    if (angle < 0):
-        return max_in_range(int(distro_size / 2 + angle * notch), 20, distro, distro_size)
-
-
-def max_in_range(mean, i, distro, distro_size):
-    min_mean = mean - i
-    max_mean = mean + i
-    if min_mean < 0:
-        min_mean = 0
-    if max_mean > (distro_size - 1):
-        max_mean = distro_size - 1
-
-    max_value = -1
-
-    for j in range(min_mean, max_mean):
-        if (max_value < distro[j]):
-            max_value = distro[j]
-
-    # print 'max', max_value, 'index', i, 'mean', mean
-    return max_value
-
-
-def init_distro():
-    with distro_lock:
-        global distro, distro_size
-        distro = [1.0 / distro_size for i in range(0, 400)]
-        # distro = [0.0001 for i in range(0,400)]
-        # distro[0] = 0.99999
-
-
-def normpdf(x, mean, sd):
-    var = float(sd) ** 2
-    pi = 3.1415926
-    denom = (2 * pi * var) ** .5
-    num = math.exp(-(float(x) - float(mean)) ** 2 / (2 * var))
-    return num / denom
-
-
-def update_distro(mean, std_dev):
-    with distro_lock:
-        global distro, distro_size
-        minimum_value = 0.0000001
-        importance = 3
-
-        other_distro = [normpdf(i, mean, std_dev) for i in range(0, distro_size)]
-
-        for i in range(0, 400):
-            if (other_distro[i] < minimum_value):
-                other_distro[i] = minimum_value
-
-        other_distro = [float(i) / sum(other_distro) for i in other_distro]
-
-        for i in range(0, 400):
-            distro[i] = importance * distro[i] + other_distro[i]
-            if (distro[i] < minimum_value):
-                distro[i] = minimum_value
-
-        distro = [float(i) / sum(distro) for i in distro]
-
-        range_array = range(0, 400)
-        plt.clf()
-        plt.cla()
-        plt.plot(range_array, distro, 'b', range_array, other_distro, 'r')  # including h here is crucial
-
-        plt.pause(0.0001)
-        return distro.index(max(distro))
 
 
 def callback(state_msg):
@@ -322,7 +169,7 @@ def callback(state_msg):
 
 
 def lookout_for_thief(state):
-    global locked_colour, waypoint_pub
+    global locked_colour, waypoint_pub, distro, distro_size
     print 'LOOKING FOR THIEF'
     which_way = state['data']['which_way']
 
@@ -333,7 +180,8 @@ def lookout_for_thief(state):
         waypoint_pub.publish(json.dumps({'id': 'FOLLOW_PERP', 'data': {'angle': -90, 'distance': 0}}))
 
     locked_colour = state['data']['colour']
-    init_distro()
+    with distro_lock:
+        distro = init_distro(distro_size)
     decide_on_thief_status()
 
     if state_id != states.ALARM:
--- src/util_modules/utils_detect.py
+++ src/util_modules/utils_detect.py
@@ -1,58 +1,89 @@
 import cv2
 import math
 import matplotlib.pyplot as plt
+import numpy
+from imutils.object_detection import non_max_suppression
+import sys
 
-def detect_avg_color(image, contours):
+colour_diff_threshold = 40
+
+
+def detect_closest_to_thief(unmodified_image, locked_colour):
     '''
-    for 1 image, and all the contours in that image
-    decide what the overall average colour is throughout the contours
+    Given an image, detect if there are people inside it.
+    If there are people in the image, return the angle and the rectangle
+    of the person which most closely match the thief by shirt colour.
+    If there is no one in the image, then we conclude we have lost the thief.
+    NOTE: If there is someone in the image despite the thief already lost
+    The detector will still pick them up and assign some probability
+    that person is the thief.
+
+    :param unmodified_image: image captured
+    :type image: cv2.image/np.ndarray
+    :return: Original image, angle to person most matching to thief,
+    if a person was found,
+    rectangle bounding box of person most matching to thief
+    :rtype: numpy.ndarray, float, bool, ((float, float), (float, float))
     '''
-    avg_b = 0
-    avg_g = 0
-    avg_r = 0
-    total = 0
-    for contour in contours:
-        if(len(contour) > 0):
-            x, y, w, h = cv2.boundingRect(contour)
-            total = w*h
-            for i in range(x, x+w-1):
-                for j in range(y, y+h-1):
-                    avg_b += image[j][i][0]
-                    avg_g += image[j][i][1]
-                    avg_r += image[j][i][2]
 
-        return (avg_b/total, avg_g/total, avg_r/total)
-    return None
+    # use hog detector
+    hog = cv2.HOGDescriptor()
+    # use default people detection
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+    drawn_on_image = unmodified_image.copy()
 
-def detect_avg_color2(image, tl_point, br_point):
-    '''
-    for 1 image, and a rect in that image
-    decide what the overall average colour is throughout the contours
-    '''
-    avg_b = 0
-    avg_g = 0
-    avg_r = 0
-    total = 0
+    # detect people in the image with hog detector
+    (rects, weights) = hog.detectMultiScale(unmodified_image, winStride=(6, 6),
+                                            padding=(16, 16), scale=1.05)
 
-    if br_point[0] > 400:
-        br_point[0] = 400
-    if br_point[1] > 300:
-        br_point[1] = 300
+    # detect_angle_to_person(image, ((x, y), (x+w, y + h)))
 
-    x = tl_point[0]
-    y = tl_point[1]
-    w = br_point[0] - tl_point[0]
-    h = br_point[1] - tl_point[1]
-    total = w*h
-    for i in range(x, x+w-1):
-        for j in range(y, y+h-1):
-            avg_b += image[j][i][0]
-            avg_g += image[j][i][1]
-            avg_r += image[j][i][2]
-    #assert total != 0?
+    angle = 0
+    lost = True
+    closest_rect = None
+    latest_rect_global = None
+    if len(rects) != 0:
+        rects = numpy.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        # apply non-maxima suppression to the bounding boxes using a
+        # fairly large overlap threshold to try to maintain overlapping
+        # boxes that are still people
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-    print ('Detected', avg_b/total, avg_g/total, avg_r/total)
-    return (avg_b/total, avg_g/total, avg_r/total)
+        closest_colour_diff = sys.maxint  # some max number
+
+        # each point in `pick` is final bounding box of person
+        for (xA, yA, xB, yB) in pick:
+            # print (xA, yA, xB, yB)
+            # take only the shirt of the person
+            new_xA = (xB - xA) / 3 + xA
+            new_yA = (yB - yA) / 4 + yA
+            new_xB = xB - (xB - xA) / 3
+            new_yB = yB - (yB - yA) / 2
+            if new_xB >= 400:
+                new_xB = 399
+            if new_yB >= 300:
+                new_yB = 299
+
+            mode_colour = get_mode_colour(unmodified_image,
+                                          new_xA, new_yA, new_xB, new_yB)
+            print 'mode', mode_colour
+            cv2.rectangle(drawn_on_image, (new_xA, new_yA), (new_xB, new_yB), (0, 255, 255), 2)
+            cv2.rectangle(drawn_on_image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+            colour_diff = euclidian_colour_diff(mode_colour, locked_colour)
+            print 'diff', colour_diff
+            # if shirt colour is more similar that what we had
+            # this person becomes the new 'closest to thief'
+            if colour_diff < closest_colour_diff:
+                closest_colour_diff = colour_diff
+                closest_rect = ((xA, yA), (xB, yB))
+                latest_rect_global = closest_rect
+                angle = detect_angle_to_person(unmodified_image, ((xA, yA), (xB, yB)))
+
+        lost = closest_colour_diff > colour_diff_threshold
+
+    return drawn_on_image, angle, lost, closest_rect, latest_rect_global
+
 
 def euclidian_colour_diff(colour1, colour2):
     gdiff = colour2[0] - colour1[0]
@@ -66,7 +97,7 @@ def euclidian_colour_diff(colour1, colour2):
     return math.sqrt(gdiff + bdiff + rdiff)
 
 
-def make_histogram(image, left, up, right, down, histogram):
+def get_mode_colour(image, left, up, right, down, histogram=False):
     b = [0 for _ in range(0, 256)]
     g = [0 for _ in range(0, 256)]
     r = [0 for _ in range(0, 256)]
@@ -95,3 +126,128 @@ def index_of_max(arr):
             max_val = arr[i]
             max_index = i
     return max_index
+
+
+def detect_angle_to_person(image, rectangle):
+    '''
+    Given an image and points describing a rectangle,
+    which represent a person,
+    calculate the angle to turn to face that person.
+
+    :param image: The image to detect from
+    :param rectangle: 2 points, top left and bottom right,
+    of a rectangle outlining a person in the image
+    :type image: cv2.image/numpy.ndarray
+    :type rectangle:((float, float),(float, float))
+    :return: angle to person from centre of camera
+    :rtype: float
+    '''
+    global distro_size, fov
+
+    top_left = rectangle[0][0]
+    bottom_right = rectangle[1][0]
+    if (bottom_right >= distro_size):
+        bottom_right = distro_size - 1
+
+    mid_image = image.shape[1] / 5.0
+
+    centre = ((top_left + bottom_right) / 2)
+
+    fixed_angle = 1
+
+    if centre <= mid_image:
+        return -fixed_angle
+    if centre > mid_image and centre < (mid_image)*4:
+        return 0
+    if centre >= (mid_image * 4):
+        return fixed_angle
+
+
+def get_distance(((xA, yA), (xB, yB)), depth_history):
+    '''
+    Get the distance to a bounding box, represents a person
+
+    :param: points, top-left and bottom-right of the bounding box
+    :type: ((float, float), (float, float))
+    :return: distance from camera to person
+    :rtype: float
+    '''
+    saved_history = depth_history[:]
+    avg_distance = 0
+    for depth_image in saved_history:
+        avgX = (xB + xA) / 2
+        avgY = (yB + yA) / 7
+        x_diff_offset = (xB - xA) * 5 / 100
+        y_diff_offset = (yB - yA) * 15 / 100
+
+        xB = avgX + x_diff_offset
+        xA = avgX - x_diff_offset
+        yB = 3 * avgY + y_diff_offset
+        yA = 3 * avgY - y_diff_offset
+        depth_image = depth_image[yA:yB, xA:xB]
+
+        values = filter(lambda a: a != 0, depth_image.flatten())
+        if (len(values) > 0):
+            avg_distance = numpy.array(values).mean()
+
+    if avg_distance == 666666:
+        return 0
+    return avg_distance / 1000.0
+
+
+def max_in_range(mean, i, distro, distro_size):
+    min_mean = mean - i
+    max_mean = mean + i
+    if min_mean < 0:
+        min_mean = 0
+    if max_mean > (distro_size - 1):
+        max_mean = distro_size - 1
+
+    max_value = -1
+
+    for j in range(min_mean, max_mean):
+        if (max_value < distro[j]):
+            max_value = distro[j]
+
+    # print 'max', max_value, 'index', i, 'mean', mean
+    return max_value
+
+
+def init_distro(distro_size):
+    return [1.0 / distro_size for i in range(0, 400)]
+
+
+def normpdf(x, mean, sd):
+    var = float(sd) ** 2
+    pi = 3.1415926
+    denom = (2 * pi * var) ** .5
+    num = math.exp(-(float(x) - float(mean)) ** 2 / (2 * var))
+    return num / denom
+
+
+def update_distro(mean, std_dev, distro, distro_size):
+    minimum_value = 0.0000001
+    importance = 3
+
+    other_distro = [normpdf(i, mean, std_dev) for i in range(0, distro_size)]
+
+    for i in range(0, 400):
+        if (other_distro[i] < minimum_value):
+            other_distro[i] = minimum_value
+
+    other_distro = [float(i) / sum(other_distro) for i in other_distro]
+
+    for i in range(0, 400):
+        distro[i] = importance * distro[i] + other_distro[i]
+        if (distro[i] < minimum_value):
+            distro[i] = minimum_value
+
+    distro = [float(i) / sum(distro) for i in distro]
+
+    range_array = range(0, 400)
+    plt.clf()
+    plt.cla()
+    plt.plot(range_array, distro, 'b', range_array, other_distro, 'r')  # including h here is crucial
+
+    plt.pause(0.0001)
+    return distro.index(max(distro))
