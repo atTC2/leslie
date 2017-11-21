2b003393936d1684dd1aa77aa2a34cba7ed8ee3d
tbc452 <tbc452@student.bham.ac.uk>
2017-11-21 19:51:03 +0000

    tweaks

--- scripts/fake_pub_alarm_handled.py
+++ scripts/fake_pub_alarm_handled.py
@@ -6,13 +6,13 @@ import fake_pub_base
 from state_machine import actions, states
 
 
-rospy.init_node('fake_pub_move_home', anonymous=True)
+rospy.init_node('fake_pub_alarm_handled', anonymous=True)
 
 fake_pub_base.block_until_state(states.ALARM_REPORT)
 
 fake_pub_base.publish(
     {
         'id': actions.ALARM_HANDLED,
-        'data': None
+        'data': {}
     }
 )
