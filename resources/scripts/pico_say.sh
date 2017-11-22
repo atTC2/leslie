8b73cc0230284a2733471af9685f1c1cd429d7c9
rrama <ben_durrans@hotmail.co.uk>
2017-11-22 05:37:46 +0000

    Add locking for `pico_say.sh`

--- resources/scripts/pico_say.sh
+++ resources/scripts/pico_say.sh
@@ -9,7 +9,11 @@ x=''
 # The name of this script.
 scriptName='pico_say.sh'
 # Float regex.
-float_regex='^[0-9]+([.][0-9]+)?$'
+floatRegex='^[0-9]+([.][0-9]+)?$'
+# Directory to lock on and work in.
+workDir="/tmp/pico_say"
+# PID file for fun.
+pidFile="${workDir}/PID"
 
 # --- --- ---
 
@@ -36,30 +40,39 @@ function error_exit
 if [ -z "${1+x}" ] || [ -z "${2+x}" ]; then
     error_exit "Usage: ./${scriptName} tempo text" 2
 fi
-if ! [[ "${1}" =~ ${float_regex} ]]; then
+if ! [[ "${1}" =~ ${floatRegex} ]]; then
     error_exit "Tempo must be a positive decimal." 2
 fi
 
 # --- --- ---
 
 
+# --- Mutex the script. ---
+
+# Acquire a lock, to ensure two speech processes aren't running at once.
+until mkdir "${workDir}" &>/dev/null
+do
+    sleep 0.5
+done
+
+function removeLock {
+    rm -rf "${workDir}"
+}
+trap removeLock EXIT
+
+echo "$$" >"${pidFile}"
+
+# --- --- ---
+
+
 # --- Main script. ---
 
-# Do our dirty work in a temporary directory.
-tmpDir="$(mktemp -d)"
-
-# Clean up on exit, always.
-function finish {
-  rm -rf "${tmpDir}"
-}
-trap finish EXIT
-
 # TTS.
-wav0="${tmpDir}/out0.wav"
+wav0="${workDir}/out0.wav"
 pico2wave -l=en-GB -w="${wav0}" "${2}"
 
 # Speed up Leslie's voice.
-wav1="${tmpDir}/out1.wav"
+wav1="${workDir}/out1.wav"
 sox "${wav0}" "${wav1}" vol 0.8 tempo "${1}"
 
 # Play the speech.
